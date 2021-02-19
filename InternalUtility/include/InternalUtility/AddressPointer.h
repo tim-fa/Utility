4646a20340e5f86d81996d34749f0838b7b51a6c
tim-fa <tfa@jcbachmann.de>
2021-02-19 01:16:40 +0100

    AddressPointer now Initializable even if address isn't valid yet

--- InternalUtility/include/InternalUtility/AddressPointer.h
+++ InternalUtility/include/InternalUtility/AddressPointer.h
@@ -13,24 +13,25 @@ class AddressPointer
 {
 	public:
 		template<typename... Args>
-		AddressPointer(Args... offsets);
+		explicit AddressPointer(Args... offsets);
 		void printPointerPath();
 		operator Type&() const;
-		Type operator=(const Type value);
+		AddressPointer<Type>& operator=(Type value);
 		long operator&();
 		Type* operator->();
 
+		long getAddress();
+		void setValue(Type value);
+
 	private:
 		void addOffset(long offset);
-
+		long calculateAddress();
 		std::vector<int> offsets;
-		long currentAddress;
 };
 
 template<typename Type>
 template<typename... Args>
 inline AddressPointer<Type>::AddressPointer(Args... offsets)
-	: currentAddress(Internals::getModuleBase())
 {
 	for (long offset : {offsets...}) {
 		addOffset(offset);
@@ -40,49 +41,73 @@ inline AddressPointer<Type>::AddressPointer(Args... offsets)
 template<typename Type>
 inline void AddressPointer<Type>::printPointerPath()
 {
-	long currentAddress = Internals::getModuleBase();
+	long curAdr = Internals::getModuleBase();
 	int i = 0;
 	for (auto& offset : offsets) {
-		long pointedValue = *(long*)(currentAddress + offset);
-		printf("Level %2d: 0x%010X + 0x%05X [0x%010X] -> 0x%X (Decimal: %d)\n", i++, currentAddress, offset, currentAddress + offset, pointedValue,
+		long pointedValue = *(long*)(curAdr + offset);
+		printf("Level %2d: 0x%010X + 0x%05X [0x%010X] -> 0x%X (Decimal: %d)\n", i++, curAdr, offset, curAdr + offset, pointedValue,
 			pointedValue);
-		currentAddress = pointedValue;
+		curAdr = pointedValue;
 	}
 }
 
 template<typename Type>
 inline void AddressPointer<Type>::addOffset(long offset)
 {
-	if (offsets.size() > 0) {
-		currentAddress = *(long*)currentAddress;
-	}
 	offsets.push_back(offset);
-	currentAddress = currentAddress + offset;
+}
+
+template<typename Type>
+inline long AddressPointer<Type>::calculateAddress()
+{
+	long currentAddress = Internals::getModuleBase();
+	bool first = true;
+
+	for (auto& offset : offsets) {
+		if (!first) {
+			currentAddress = *(long*)currentAddress;
+		}
+		currentAddress = currentAddress + offset;
+		first = false;
+	}
+	return currentAddress;
 }
 
 template<typename Type>
 inline AddressPointer<Type>::operator Type&() const
 {
-	return *(Type*)currentAddress;
+	return *(Type*)getAddress();
 }
 
 template<typename Type>
-inline Type AddressPointer<Type>::operator=(const Type value)
+inline AddressPointer<Type>& AddressPointer<Type>::operator=(Type value)
 {
-	*(Type*)currentAddress = value;
-	return value;
+	setValue(value);
+	return *this;
 }
 
 template<typename Type>
 inline long AddressPointer<Type>::operator&()
 {
-	return currentAddress;
+	return getAddress();
 }
 
 template<typename Type>
 inline Type* AddressPointer<Type>::operator->()
 {
-	return (Type*)currentAddress;
+	return (Type*)getAddress();
+}
+
+template<typename Type>
+long AddressPointer<Type>::getAddress()
+{
+	return calculateAddress();
+}
+
+template<typename Type>
+void AddressPointer<Type>::setValue(Type value)
+{
+	*(Type*)calculateAddress() = value;
 }
 }
 
