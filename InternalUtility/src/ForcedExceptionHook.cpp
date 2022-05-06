5bc2796cdd39b95f0c09bbe215724e72f5d7bee1
Tim Farahani <tim.farahani@gmail.com>
2022-05-06 18:59:44 +0200

    FEH Hook now works with multiple hooks

--- InternalUtility/src/ForcedExceptionHook.cpp
+++ InternalUtility/src/ForcedExceptionHook.cpp
@@ -8,47 +8,76 @@
 namespace Hook
 {
 
+void printRegisters(EXCEPTION_POINTERS* exceptionInfo)
+{
+	Log::Logger::Error("Code: 0x{:X}", exceptionInfo->ExceptionRecord->ExceptionCode);
+	Log::Logger::Info("EIP: 0x{:X}", exceptionInfo->ContextRecord->Eip);
+	Log::Logger::Warning("EBP: 0x{:X}", exceptionInfo->ContextRecord->Ebp);
+	Log::Logger::Warning("ESI: 0x{:X}", exceptionInfo->ContextRecord->Esi);
+	Log::Logger::Warning("EAX: 0x{:X}", exceptionInfo->ContextRecord->Eax);
+	Log::Logger::Warning("EDX: 0x{:X}", exceptionInfo->ContextRecord->Edx);
+	Log::Logger::Warning("ECX: 0x{:X}", exceptionInfo->ContextRecord->Ecx);
+	Log::Logger::Warning("EDI: 0x{:X}", exceptionInfo->ContextRecord->Edi);
+	Log::Logger::Warning("ESP: 0x{:X}", exceptionInfo->ContextRecord->Esp);
+}
+
 LONG WINAPI crashHandler(EXCEPTION_POINTERS* exceptionInfo)
 {
 	if (exceptionInfo->ExceptionRecord->ExceptionCode != EXCEPTION_ACCESS_VIOLATION) {
 		return EXCEPTION_CONTINUE_SEARCH;
 	}
 
+	bool fixed = false;
 	for (auto& ctx: ForcedExceptionHook::hookContexts) {
 		if (exceptionInfo->ContextRecord->Ebp == ctx.exceptionValue) {
+			if (ctx.debugOutput) {
+				printRegisters(exceptionInfo);
+			}
 			exceptionInfo->ContextRecord->Ebp = ctx.restoreValue;
-		} else if (exceptionInfo->ContextRecord->Esi == ctx.exceptionValue) {
-			exceptionInfo->ContextRecord->Esi = ctx.restoreValue;
-		} else if (exceptionInfo->ContextRecord->Eax == ctx.exceptionValue) {
-			exceptionInfo->ContextRecord->Eax = ctx.restoreValue;
-		} else if (exceptionInfo->ContextRecord->Edx == ctx.exceptionValue) {
-			exceptionInfo->ContextRecord->Edx = ctx.restoreValue;
-		} else if (exceptionInfo->ContextRecord->Ecx == ctx.exceptionValue) {
-			exceptionInfo->ContextRecord->Ecx = ctx.restoreValue;
-		} else if (exceptionInfo->ContextRecord->Edi == ctx.exceptionValue) {
-			exceptionInfo->ContextRecord->Edi = ctx.restoreValue;
-		} else if (exceptionInfo->ContextRecord->Esp == ctx.exceptionValue) {
-			exceptionInfo->ContextRecord->Esp = ctx.restoreValue;
-		} else {
-			Log::Logger::Fatal("Could not restore from exception!");
-			exit(-1);
+			fixed = true;
+		}
+		if (exceptionInfo->ContextRecord->Esi == ctx.exceptionValue) {
+			if (ctx.debugOutput) {
+				printRegisters(exceptionInfo);
+			}
+			exceptionInfo->ContextRecord->Esi = ctx.restoreValue;			fixed = true;
+		}
+		if (exceptionInfo->ContextRecord->Eax == ctx.exceptionValue) {
+			if (ctx.debugOutput) {
+				printRegisters(exceptionInfo);
+			}
+			exceptionInfo->ContextRecord->Eax = ctx.restoreValue;			fixed = true;
+		}
+		if (exceptionInfo->ContextRecord->Edx == ctx.exceptionValue) {
+			if (ctx.debugOutput) {
+				printRegisters(exceptionInfo);
+			}
+			exceptionInfo->ContextRecord->Edx = ctx.restoreValue;			fixed = true;
+		}
+		if (exceptionInfo->ContextRecord->Ecx == ctx.exceptionValue) {
+			if (ctx.debugOutput) {
+				printRegisters(exceptionInfo);
+			}
+			exceptionInfo->ContextRecord->Ecx = ctx.restoreValue;			fixed = true;
+		}
+		if (exceptionInfo->ContextRecord->Edi == ctx.exceptionValue) {
+			if (ctx.debugOutput) {
+				printRegisters(exceptionInfo);
+			}
+			exceptionInfo->ContextRecord->Edi = ctx.restoreValue;			fixed = true;
+		}
+		if (exceptionInfo->ContextRecord->Esp == ctx.exceptionValue) {
+			if (ctx.debugOutput) {
+				printRegisters(exceptionInfo);
+			}
+			exceptionInfo->ContextRecord->Esp = ctx.restoreValue;			fixed = true;
 		}
 		if (exceptionInfo->ContextRecord->Eip == ctx.expectedExceptionAddress) {
 			ctx.callback(exceptionInfo);
 		}
-
-		if (ctx.debugOutput) {
-			Log::Logger::Debug("Code: 0x{:X}", exceptionInfo->ExceptionRecord->ExceptionCode);
-			Log::Logger::Debug("EIP: 0x{:X}", exceptionInfo->ContextRecord->Eip);
-			Log::Logger::Debug("EBP: 0x{:X}", exceptionInfo->ContextRecord->Ebp);
-			Log::Logger::Debug("ESI: 0x{:X}", exceptionInfo->ContextRecord->Esi);
-			Log::Logger::Debug("EAX: 0x{:X}", exceptionInfo->ContextRecord->Eax);
-			Log::Logger::Debug("EDX: 0x{:X}", exceptionInfo->ContextRecord->Edx);
-			Log::Logger::Debug("EXC: 0x{:X}", exceptionInfo->ContextRecord->Ecx);
-			Log::Logger::Debug("EDI: 0x{:X}", exceptionInfo->ContextRecord->Edi);
-			Log::Logger::Debug("ESP: 0x{:X}", exceptionInfo->ContextRecord->Esp);
-		}
-		break;
+	}
+	if (!fixed){
+		Log::Logger::Fatal("Access violation can't be fixed!");
 	}
 	return EXCEPTION_CONTINUE_EXECUTION;
 }
