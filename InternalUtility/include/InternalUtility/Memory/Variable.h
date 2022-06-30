0a79a1db01f8d0bf783850d64b88be0c60129b07
Tim Farahani <tim.farahani@gmail.com>
2022-06-30 12:49:38 +0200

    InternalUtility: Variable Index Operator exclusively for array definitions

--- InternalUtility/include/InternalUtility/Memory/Variable.h
+++ InternalUtility/include/InternalUtility/Memory/Variable.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <vector>
 #include <stdexcept>
 #include <exception>
@@ -13,163 +14,115 @@
 
 namespace Hooking
 {
-template<typename Type = long, long TypeSize = 0>
+template<typename DataType>
 class Variable
 {
 	public:
 		Variable() = default;
 
 		template<typename... Args>
-		explicit Variable(Args... offsets);
+		explicit Variable(Args... offsets)
+			: offs({offsets...})
+		{
+		}
 
 		template<typename... Args>
-		void initializeByOffsets(Args... offsets);
-		void initializeByPattern(const std::string& pattern);
+		void initializeByOffsets(Args... offsets)
+		{
+			offs.clear();
+			for (auto& offset: {offsets...}) {
+				offs.push_back(offset);
+			}
+		}
+		void initializeByPattern(const std::string& pattern)
+		{
+			initializeByOffsets(findPlaceholderAddress(pattern) - getModuleBase());
+		}
+		void printPointerPath()
+		{
+			BaseType_t curAdr = getModuleBase();
+			int i = 0;
+			for (auto& offset: offs) {
+				BaseType_t pointedValue = *(BaseType_t*)(curAdr + offset);
+				Log::Logger::d("Level {:2d}: 0x{:010X} + 0x{:06X} [0x{:010X}] -> 0x{:X} (Decimal: {:d})\n", i++, curAdr, offset,
+					curAdr + offset, pointedValue, pointedValue);
+				curAdr = pointedValue;
+			}
+		}
+		operator DataType&()
+		{
+			return *(DataType*)getAddress();
+		}
 
-		void printPointerPath();
+		Variable<DataType>& operator=(DataType value)
+		{
+			setValue(value);
+			return *this;
+		}
+		DataType operator&=(DataType value)
+		{
+			*(DataType*)getAddress() &= value;
+			return *(DataType*)getAddress();
+		}
+		DataType* operator&()
+		{
+			return (DataType*)getAddress();
+		}
+		DataType* operator->()
+		{
+			return (DataType*)getAddress();
+		}
+		BaseType_t getAddress()
+		{
+			__try
+			{
+				initializePointer();
+			}
+			__except(EXCEPTION_EXECUTE_HANDLER)
+			{
+				throw std::runtime_error("Variable::getAddress(): Access violation");
+			}
+			return currentAddress;
+		}
 
-		operator Type&();
-
-		Variable<Type, TypeSize>& operator=(Type value);
-
-		Type operator&=(Type value);
-
-		Type* operator&();
-
-		Type* operator->();
-
-		Type& operator[](int index);
-
-		BaseType_t getAddress();
-
-		void setValue(Type value);
+		void setValue(DataType value)
+		{
+			*(DataType*)getAddress() = value;
+		}
 
 	private:
-		void initializePointer();
-
-		void addOffset(long offset);
+		void initializePointer()
+		{
+			currentAddress = getModuleBase();
+			bool first = true;
+			for (auto& offset: offs) {
+				if (!first) {
+					currentAddress = *(BaseType_t*)currentAddress;
+				}
+				currentAddress = currentAddress + offset;
+				first = false;
+			}
+			*(BaseType_t*)currentAddress++;
+			*(BaseType_t*)currentAddress--;
+		}
+		void addOffset(long offset)
+		{
+			offs.push_back(offset);
+		}
 
 		BaseType_t currentAddress{0};
 		std::vector<int> offs;
 };
 
-template<typename Type, long TypeSize>
-template<typename... Args>
-inline Variable<Type, TypeSize>::Variable(Args... offsets)
-	: offs({offsets...})
-{
-}
 
-template<typename Type, long TypeSize>
-template<typename... Args>
-inline void Variable<Type, TypeSize>::initializeByOffsets(Args... offsets)
+template<typename DataType>
+class Variable<DataType[]> : public Variable<DataType>
 {
-	offs.clear();
-	for (auto& offset: {offsets...}) {
-		offs.push_back(offset);
-	}
-}
-
-template<typename Type, long TypeSize>
-inline void Variable<Type, TypeSize>::printPointerPath()
-{
-	BaseType_t curAdr = getModuleBase();
-	int i = 0;
-	for (auto& offset: offs) {
-		BaseType_t pointedValue = *(BaseType_t*)(curAdr + offset);
-		Log::Logger::d("Level {:2d}: 0x{:010X} + 0x{:06X} [0x{:010X}] -> 0x{:X} (Decimal: {:d})\n", i++, curAdr, offset,
-			curAdr + offset, pointedValue, pointedValue);
-		curAdr = pointedValue;
-	}
-}
-
-template<typename Type, long TypeSize>
-inline void Variable<Type, TypeSize>::addOffset(long offset)
-{
-	offs.push_back(offset);
-}
-
-template<typename Type, long TypeSize>
-inline Variable<Type, TypeSize>::operator Type&()
-{
-	return *(Type*)getAddress();
-}
-
-template<typename Type, long TypeSize>
-inline Variable<Type, TypeSize>& Variable<Type, TypeSize>::operator=(Type value)
-{
-	setValue(value);
-	return *this;
-}
-
-template<typename Type, long TypeSize>
-inline Type* Variable<Type, TypeSize>::operator->()
-{
-	return (Type*)getAddress();
-}
-
-template<typename Type, long TypeSize>
-inline Type* Variable<Type, TypeSize>::operator&()
-{
-	return (Type*)getAddress();
-}
-
-template<typename Type, long TypeSize>
-inline Type& Variable<Type, TypeSize>::operator[](int index)
-{
-	int size = TypeSize;
-	if (!TypeSize) {
-		size = sizeof(Type);
-	}
-	return *(Type*)(getAddress() + size * index);
-}
-
-template<typename Type, long TypeSize>
-BaseType_t Variable<Type, TypeSize>::getAddress()
-{
-	__try
-	{
-		initializePointer();
-	}
-	__except(EXCEPTION_EXECUTE_HANDLER)
-	{
-		throw std::runtime_error("Variable::getAddress(): Access violation");
-	}
-	return currentAddress;
-}
-
-template<typename Type, long TypeSize>
-void Variable<Type, TypeSize>::setValue(Type value)
-{
-	*(Type*)getAddress() = value;
-}
-
-template<typename Type, long TypeSize>
-void Variable<Type, TypeSize>::initializePointer()
-{
-	currentAddress = getModuleBase();
-	bool first = true;
-	for (auto& offset: offs) {
-		if (!first) {
-			currentAddress = *(BaseType_t*)currentAddress;
+	public:
+		DataType& operator[](int index)
+		{
+			return *(DataType*)(Variable<DataType>::getAddress() + sizeof(DataType) * index);
 		}
-		currentAddress = currentAddress + offset;
-		first = false;
-	}
-	*(BaseType_t*)currentAddress++;
-	*(BaseType_t*)currentAddress--;
-}
+};
 
-template<typename Type, long TypeSize>
-Type Variable<Type, TypeSize>::operator&=(Type value)
-{
-	*(Type*)getAddress() &= value;
-	return *(Type*)getAddress();
-}
-
-template<typename Type, long TypeSize>
-inline void Variable<Type, TypeSize>::initializeByPattern(const std::string& pattern)
-{
-	initializeByOffsets(findPlaceholderAddress(pattern) - getModuleBase());
-}
 }
