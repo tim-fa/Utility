f12d15bcd27701e3faeb96b19fc9b6129c0440b2
tim-fa <tim.farahani@gmail.com>
2022-09-28 20:06:12 +0200

    Improved hash generator and hashed memory for Obfuscator

---
+++ Obfuscation/include/Obfuscation/HashedMemory.h
@@ -0,0 +1,78 @@
+#pragma once
+
+
+#include <memory>
+
+
+static int randomInt() {
+    static std::random_device dev;
+    static std::mt19937 rng(dev());
+    static std::uniform_int_distribution<std::mt19937::result_type> dist(1, 2 * INT_MAX);
+    return dist(rng);
+}
+
+
+class HashedMemory {
+public:
+    HashedMemory(int size)
+            : m_memorySize(size * 2 + 1), m_dataMemory(new unsigned char[m_memorySize]), m_initialized(false) {
+    }
+
+    __forceinline int getMemoryHash() {
+        size_t fullMapHash = 0;
+        for (int dataIdx = 0; dataIdx < floor(m_memorySize / 2); dataIdx++) {
+            static std::hash<int> hg;
+            fullMapHash += hg(m_dataMemory[dataIdx * 2]);
+            fullMapHash += hg(m_dataMemory[dataIdx * 2 + 1]);
+            //fullMapHash += hg(m_hashSeeds[dataIdx]);
+        }
+        return fullMapHash;
+    }
+
+    __forceinline void writeMemoryHash()
+    {
+        m_lastMapHash = 0;
+        for (int dataIdx = 0; dataIdx < floor(m_memorySize / 2); dataIdx++) {
+            static std::hash<int> hg;
+            m_dataMemory[dataIdx * 2 + 1] = static_cast<unsigned char>(randomInt());
+            m_lastMapHash += hg(m_dataMemory[dataIdx * 2]);
+            m_lastMapHash += hg(m_dataMemory[dataIdx * 2 + 1]);
+           // m_lastMapHash += hg(m_hashSeeds[dataIdx]);
+        }
+    }
+    __forceinline void write(unsigned char *data) {
+        m_hashSeeds.clear();
+        if (getMemoryHash() != m_lastMapHash && m_initialized){
+            throw std::runtime_error("Illegal write operation on data!");
+        }
+        m_initialized = true;
+        for (int dataIdx = 0; dataIdx < floor(m_memorySize / 2); dataIdx++) {
+            memcpy_s(&m_dataMemory[dataIdx * 2], 1, &data[dataIdx], 1);
+            m_hashSeeds.push_back(randomInt());
+            m_dataMemory[dataIdx * 2] = ~m_dataMemory[dataIdx * 2] ^ m_hashSeeds.back();
+            static std::hash<unsigned char> hg;
+            m_dataMemory[dataIdx * 2 + 1] = static_cast<unsigned char>(randomInt());
+        }
+        writeMemoryHash();
+    }
+
+    __forceinline std::shared_ptr<unsigned char[]> read() {
+        if (getMemoryHash() != m_lastMapHash && m_initialized){
+            throw std::runtime_error("Illegal write operation on data!");
+        }
+        std::shared_ptr<unsigned char[]> result(new unsigned char[floor(m_memorySize / 2)]);
+        for (int dataIdx = 0; dataIdx < floor(m_memorySize / 2); dataIdx++) {
+            memcpy_s(&result[dataIdx], 1, &m_dataMemory[dataIdx * 2], 1);
+            static std::hash<unsigned char> hg;
+            result[dataIdx] = ~result[dataIdx] ^ m_hashSeeds[dataIdx];
+        }
+        return result;
+    }
+
+private:
+    const int m_memorySize;
+    std::shared_ptr<unsigned char[]> m_dataMemory;
+    size_t m_lastMapHash;
+    bool m_initialized;
+    std::vector<int> m_hashSeeds;
+};
--- Obfuscation/include/Obfuscation/Obfuscator.h
+++ Obfuscation/include/Obfuscation/Obfuscator.h
@@ -2,81 +2,29 @@
 #include <format>
 #include <random>
 #include <sstream>
+#include "HashedMemory.h"
 
 namespace Obfuscation
 {
-enum SplitCount : int
-{
-	Split2 = 2,
-	Split4 = 4,
-	Split8 = 8
-};
 
-int randomInt()
-{
-	static std::random_device dev;
-	static std::mt19937 rng(dev());
-	static std::uniform_int_distribution<std::mt19937::result_type> dist(1, 2 * INT_MAX);
-	return dist(rng);
-}
 
 template<typename Type>
 class Obfuscator
 {
 	public:
-		explicit Obfuscator(Type value = 0, SplitCount splitCount = SplitCount::Split2)
-			: bitMasks(new int[splitCount])
-			, bitStartIndex(new short[splitCount])
-			, data(new int[splitCount])
-			, splitCount(splitCount)
+		explicit Obfuscator(Type value = 0)
+			: memory(sizeof(Type))
 		{
-			verificationHash = randomInt();
-
-			int varBits = sizeof(Type) * 8;
-			if (splitCount > varBits) {
-				throw std::runtime_error("A " + std::to_string(varBits) + "-Bit Variable can not be split in to more than " + std::to_string(varBits) + " individual Variables");
-			}
-			bitsPerSlot =  sizeof(Type) * 8 / splitCount;
-
-			for (int maskIdx = 0; maskIdx < splitCount; maskIdx++) {
-				bitMasks[maskIdx] = 0;
-				for (int i = 0; i < bitsPerSlot; i++) {
-					bitMasks[maskIdx] |= (1 << (i + bitsPerSlot * maskIdx));
-				}
-			}
-			set(value);
 		}
 
 		__forceinline void set(Type value)
 		{
-			static std::hash<unsigned int> bitsHash;
-			for (int i = 0; i < splitCount; i++) {
-				bitStartIndex[i] = randomInt() % (sizeof(int) * 8 - bitsPerSlot);
-				unsigned int bitsToSetMask = bitMasks[0] << bitStartIndex[i];
-				unsigned int bitsToSet = ~value & bitMasks[i];
-				// Set the data bits at the correct location
-				data[i] = ((bitsToSet >> bitsPerSlot * i) << bitStartIndex[i]);
-				unsigned int rawBitsAlignedRight = value >> (bitsPerSlot * i) & bitMasks[0];
-				auto bitHashValue = static_cast<unsigned int>(bitsHash(rawBitsAlignedRight));
-				data[i] |= (verificationHash ^ bitHashValue) & ~bitsToSetMask;
-			}
+			memory.write(reinterpret_cast<unsigned char*>(&value));
 		}
 
 		__forceinline Type get()
 		{
-			static std::hash<unsigned int> bitsHash;
-			Type returnValue = 0;
-			for (int slotIdx = 0; slotIdx < splitCount; slotIdx++) {
-				unsigned int dataBits = (~data[slotIdx] >> bitStartIndex[slotIdx]) & bitMasks[0];
-				unsigned int readBitsMask = bitMasks[0] << bitStartIndex[slotIdx];
-				unsigned int actualHash = data[slotIdx] & ~readBitsMask;
-				unsigned int calculatedHash = (verificationHash ^ static_cast<unsigned int>(bitsHash(dataBits))) & ~readBitsMask;
-				if (calculatedHash != actualHash) {
-					throw std::runtime_error("Value was modified illegally");
-				}
-				returnValue |= dataBits << bitsPerSlot * slotIdx;
-			}
-			return returnValue;
+			return *(Type*)memory.read().get();
 		}
 
 		__forceinline operator Type()
@@ -117,11 +65,6 @@ class Obfuscator
 		}
 
 	private:
-		SplitCount splitCount;
-		int bitsPerSlot;
-		int verificationHash;
-		std::shared_ptr<int[]> bitMasks;
-		std::shared_ptr<short[]> bitStartIndex;
-		std::shared_ptr<int[]> data;
+		HashedMemory memory;
 };
 }
